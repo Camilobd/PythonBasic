fcda3b2194c38e02aa1b4799ba9ab647fabd9a24
Camilo Bohorquez Dallos <camilo.bohorquez@uptc.edu.co>
2025-05-09 15:56:38 -0500

    SI Completo

--- Codigo/registro_clase.py
+++ Codigo/registro_clase.py
@@ -1,24 +1,69 @@
-documento=""
-nombre=""
+#documento="" esto es una variable
+from random import randint
+
+nombre_bd=["Camilo", "Juan", "Pedro", "Luis", "Andres", "Alejandra","Marcela","Michael","Yulieth","Felipe"]
+documento=[]# Arreglo de datos 
+nombre=[]
 oper=0
 
-while oper!=3:
+while oper!=5:
     print("1. registro de clase")
     print("2. Mostrar asistentes a clase")
-    print("3. salir")
+    print("3. Borrar registro")
+    print("4. BUscar registro")
+    print("5. salir")
+    
     oper=(int)(input("Seleccione la opcion: "))
+
 
     if oper==1:
         print("--------- REGISTRO ESTUDIANTES ----------")
         
-        documento=input("Digite el documento para el registro")
-        nombre=input("Digite el nombre para el registro: ")
+        #documento_registro=input("Digite el documento para el registro: ")
+        
+        #nombre_registro=input("Digite el nombre para el registro: ")
+
+        for i in range(0,1000):
+            documento_registro=randint(1000000,1200000)
+            nombre_registro=nombre_bd[randint(0,(len(nombre_bd)-1))]
+
+            documento.append(documento_registro)
+            nombre.append(nombre_registro)
+       
+
 
     if oper==2:
         print("--------- MOSTRAR ESTUDIANTES ----------")
-        print ("Estudiante - Nro Documento:", documento," Nombre: ", nombre)
 
-    if oper >3:
+        for i in range(len(documento)):
+            print ("Estudiante Nro: ", i, " - Nro Documento:", 
+                   documento[i]," Nombre: ", nombre[i])
+
+    if oper==3:
+        print ("-----------BORRAR REGISTRO ESTUDIANTES -----------")
+        
+        for i in range(len(documento)):
+            print ("Estudiante Nro: ", i, " - Nro Documento:", 
+                   documento[i]," Nombre: ", nombre[i])
+        
+        pos=(int)(input("Digite el Nro de Estudiante a borrar:"))
+        documento.pop(pos)
+        nombre.pop(pos)
+
+    if oper==4:
+        print ("---------------- BUSCAR REGISTROS-------------")
+        docu=(int)(input("Digite el documento a buscar: "))
+        estado=False
+        for i in range(len(documento)):
+            if docu==documento[i]:
+                print("el nombre del estudiante que esta buscando es:",nombre[i])
+                estado=True
+            #else:
+                #print("el estudiante no se encuentra en la lista")
+        if estado==False:
+            print("el estudiante no se encuentra en la lista")
+
+    if oper >5:
         print("ESTA OPCION NO ESTA DISPONIBLE")
 
 
